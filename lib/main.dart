4b8ae0fd6c8facbdd35e7b031f2dfcf2ac39dcff
Richard Ndukwe <richie.godisgood@gmail.com>
2026-01-14 21:21:31 -0500

    Added some containers, rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -30,18 +30,130 @@ class MainApp extends StatelessWidget {
              )
           ],
         ),
-        body: Container(width: double.infinity,
-          height: 150.0, 
-          color: Colors.green, 
-          margin: EdgeInsets.symmetric(horizontal: 20.0),
-          padding: EdgeInsets.only(top: 25.0),
-          child: Container(
-            width: 300.0, 
-            height: 100.0, 
-            color: Colors.red, 
-          ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              width: double.infinity,
+              height: 170,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 182, 170, 137),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sleep',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,)
+                        ),
+                        Icon(
+                          Icons.chevron_right_rounded, color: Colors.white,),
+                    ],
+                  ),
+                  Text('By Allison S', style: TextStyle(color: Colors.grey),),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          width: 100,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 50, 50, 50),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text('very relaxing', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          width: 80,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 50, 50, 50),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text('pleasent', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          width: 60,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 50, 50, 50),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text('deep', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        height: 170,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 182, 170, 137),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Timer',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,)
+                                  ),
+                                  Icon(
+                                    Icons.chevron_right_rounded, color: Colors.white,),
+                              ],
+                            ),
+                            Text('40 mins', style: TextStyle(color: Colors.grey),),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        height: 170,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 182, 170, 137),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        height: 170,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 182, 170, 137),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ],
+                  ),
+                ), 
+              ],   
+            ), 
+          ],
         ),
-      )
+      ),
     );
   }
 }
