bc84687fb310113d24932eac3a573d72718bec77
Richard Ndukwe <richie.godisgood@gmail.com>
2026-01-24 12:12:19 -0500

    Making a new container

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,36 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('New App Bar'),
-          leading: Icon(
+          title: const Text('New App Bar'),
+          leading: const Icon(
             Icons.favorite,
             color: Colors.pink,
             size: 24.0,
             semanticLabel: 'Text to announce in accessibility modes',
-            
           ),
           elevation: 4,
           actions: [
-            IconButton(onPressed: () {},
-             icon: Icon(Icons.home, color: Colors.blue, grade:2)
-             ),
-             IconButton(onPressed: () {},
-             icon: Icon(Icons.settings, color: Colors.red)
-             )
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home, color: Colors.blue),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings, color: Colors.red),
+            ),
           ],
         ),
+
         body: Column(
           children: [
+            // First Container
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               width: double.infinity,
               height: 170,
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 182, 170, 137),
+                color: Colors.grey,
                 borderRadius: BorderRadius.circular(30),
               ),
               child: Column(
@@ -46,114 +49,206 @@ class MainApp extends StatelessWidget {
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
+                    children: const [
                       Text(
                         'Sleep',
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.white,)
-                        ),
-                        Icon(
-                          Icons.chevron_right_rounded, color: Colors.white,),
-                    ], //children
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                      Icon(Icons.chevron_right_rounded, color: Colors.white),
+                    ],
                   ),
-                  Text('By Allison S', style: TextStyle(color: Colors.grey)),
+                  const Text(
+                    'By Allison S',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                   Padding(
                     padding: const EdgeInsets.only(top: 10),
                     child: Row(
                       children: [
                         Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
                           width: 100,
                           height: 20,
                           decoration: BoxDecoration(
                             color: const Color.fromARGB(255, 50, 50, 50),
                             borderRadius: BorderRadius.circular(30),
                           ),
-                          child: Text('very relaxing', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                          child: const Text(
+                            'very relaxing',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                         Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
                           width: 80,
                           height: 20,
                           decoration: BoxDecoration(
                             color: const Color.fromARGB(255, 50, 50, 50),
                             borderRadius: BorderRadius.circular(30),
                           ),
-                          child: Text('pleasent', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                          child: const Text(
+                            'pleasant',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                         Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
                           width: 60,
                           height: 20,
                           decoration: BoxDecoration(
                             color: const Color.fromARGB(255, 50, 50, 50),
                             borderRadius: BorderRadius.circular(30),
                           ),
-                          child: Text('deep', style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                          child: const Text(
+                            'deep',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ], //children
+                      ],
                     ),
                   ),
-                ], //children
+                ],
               ),
             ),
-                  Column(
+            // 2nd Container
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              width: double.infinity,
+              height: 170,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        width: double.infinity,
-                        height: 170,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 182, 170, 137),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Timer',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,)
-                                  ),
-                                  Icon(
-                                    Icons.chevron_right_rounded, color: Colors.white,),
-                              ],
-                            ),
-                            Text('40 mins', style: TextStyle(color: Colors.grey),),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        width: double.infinity,
-                        height: 170,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 182, 170, 137),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
+                      Text(
+                        'Timer',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
                       ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        width: double.infinity,
-                        height: 170,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 182, 170, 137),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                    ], //children
+                      Icon(Icons.chevron_right_rounded, color: Colors.white),
+                    ],
                   ),
-                ), 
-              ],  //children
-            ), 
-          ], //children
+                  Text('40 mins', style: TextStyle(color: Colors.grey)),
+                ],
+              ),
+            ),
+            // 3rd Container
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              width: double.infinity,
+              height: 170,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Foundation',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      Icon(Icons.chevron_right_rounded, color: Colors.white),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.white
+                        ),
+                        child: const Text('Relaxtion'),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.white
+                        ),
+                        child: const Text('Trance'),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // 4th Container
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              width: double.infinity,
+              height: 170,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Image.asset('assets/leaf.jpg'),
+                      ),
+                      Text(
+                        'Motive',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Icon(Icons.chevron_right_rounded, color: Colors.white),
+                          Icon(Icons.download, color: Colors.white),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('Pop- Epic', style: TextStyle(color: Colors.grey)),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text('Northen Bay', style: TextStyle(color: Colors.white)),
+                ],
+              )
+            )
+          ],
         ),
+
+        // Play Button
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                fixedSize: const Size(double.infinity, 30),
+              ),
+              child: const Text('play'),
+            ),
+          ),
+        ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerDocked,
       ),
     );
-  } 
-} 
+  }
+}
